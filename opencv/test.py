8332238e3b2356db192eed62c6d77aad5e993854
DanManN <dnahimov@gmail.com>
2018-10-12 17:54:15 -0400

    smoother tracking

--- opencv/test.py
+++ opencv/test.py
@@ -6,8 +6,8 @@ from time import time
 def distance(o1, o2):
     (x1,y1,w1,h1) = o1
     (x2,y2,w2,h2) = o2
-    c1 = ((x1+w1)/2,(y1+h1)/2)
-    c2 = ((x2+w2)/2,(y2+h2)/2)
+    c1 = (x1+w1/2,y1+h1/2)
+    c2 = (x2+w2/2,y2+h2/2)
     return np.hypot(c1[0]-c2[0],c1[1]-c2[1])
 
 cv2.namedWindow("preview")
@@ -30,50 +30,45 @@ glass_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 reye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 leye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
 
-prevfaces = []
-preveyes = []
-
+face = None
+eye = None
+flost = 0
+elost = 0
 while rval:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    hasface = False;
-    haseyes = False;
-    for (x,y,w,h) in faces:
-        if len(prevfaces) > 0:
-            # print("Face: " + str(distance((x,y,w,h),prevfaces[0])))
-            if (distance((x,y,w,h),prevfaces[0])):
+    flost = flost+1
+    elost = elost+1
+    for f in faces:
+        if face is not None:
+            # print("Face: " + str(distance(f,face)))
+            if not (1 < distance(f,face) < 20):
                 continue
-        if hasface:
-            continue
+        face = f
+        flost = 0
 
-        prevfaces = faces
-        hasface = True
+    if flost < 5 and face is not None:
+        (x,y,w,h) = face
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            if len(preveyes) > 0:
-                print("Eyes: " + str(distance((ex,ey,ew,eh),preveyes[0])))
-                if (distance((ex,ey,ew,eh),preveyes[0]) > 25):
+        for e in eyes:
+            if eye is not None:
+                # print("Eyes: " + str(distance(eye,e)))
+                if not (0 < distance(e,eye) < 10):
                     continue
-            if haseyes:
-                continue
+            eye = e
+            elost = 0
 
-            haseyes = True
-            preveyes = eyes
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    if not hasface:
-        if len(prevfaces) > 0:
-            (x,y,w,h) = prevfaces[0]
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex,ey,ew,eh) in eyes:
-                if not haseyes:
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            if elost < 5:
+                (ex,ey,ew,eh) = e
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            else:
+                eye = None
+    else:
+        face = None
+        eye = None
 
 
     cv2.imshow("preview", frame)
